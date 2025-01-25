87a9ccfb066ee3e3de6e6a157209fd7bbb936217
Dev-sungmin <sungminmail0@naver.com>
2025-01-25 12:25:31 +0900

    try CI/CD workflow for Serverless app deployment

---
+++ handler.js
@@ -0,0 +1,12 @@
+// handler.js
+
+module.exports.hello = async (event) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Hello from Lambda!',
+        input: event,
+      }),
+    };
+  };
+  
